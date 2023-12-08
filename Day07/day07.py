b99f9c488f83d5b3a1a93c2c94f7f6e914fff456
Advic <loktinmartinho@gmail.com>
2023-12-07 22:21:43 -0800

    Day 7 Part 2

--- Day07/day07.py
+++ Day07/day07.py
@@ -2,6 +2,7 @@ import collections
 
 import re
 from functools import cmp_to_key
+from collections import Counter
 
 
 # class STRENGTH(Enum):
@@ -47,16 +48,61 @@ class Hand:
                 return Hand.CARDSTRENGTH[a.cards[i]] - Hand.CARDSTRENGTH[b.cards[i]]
 
 
+class JokerHand:
+    TYPEMAP = collections.OrderedDict({
+        6: r'(.)\1{4,}',
+        5: r'(.)\1{3,}',
+        4: r'(?:(.)\1{1,}(.)\2{2,}|(.)\3{2,}(.)\4{1,})',
+        3: r'(.)\1{2,}',
+        2: r'(.)\1{1,}.?(.)\2{1,}',
+        1: r'(.)\1{1,}',
+        0: r'\w{5}',
+    })
+
+    CARDSTRENGTH = {sym: i for i, sym in enumerate(reversed('AKQT98765432J'))}
+
+    def __init__(self, data: str):
+        cards, bid = data.split(' ', 1)
+        self.bid = int(bid)
+        self.cards = cards
+
+    @property
+    def type(self) -> int:
+        # Replace J with the most frequent non-J letter
+        result = Counter(self.cards.replace('J', ''))
+        mode = max(result, key=result.get) if result else 'J'
+        # print('cards:', self.cards, 'mode:', mode)
+        jokers_subbed = self.cards.replace('J', mode)
+        # if ('J' in self.cards):
+        #     print(f"%s, J->%s, %s" % (self.cards, mode, jokers_subbed))
+
+        for value, pattern in self.TYPEMAP.items():
+            if re.search(pattern, ''.join(sorted(jokers_subbed))):
+                return value
+
+    @staticmethod
+    def cmp(a, b) -> int:
+        if a.type != b.type:
+            return a.type - b.type
+        for i in range(5):
+            if a.cards[i] != b.cards[i]:
+                return JokerHand.CARDSTRENGTH[a.cards[i]] - JokerHand.CARDSTRENGTH[b.cards[i]]
+
+
 def part_1(data: str) -> int:
     total = 0
     for rank, hand in enumerate(sorted(map(Hand, data.splitlines()), key=cmp_to_key(Hand.cmp)), 1):
         total += (rank * hand.bid)
-        print(rank, hand.cards, hand.bid)
+        # print(rank, hand.cards, hand.bid)
     return total
 
 
 def part_2(data: str) -> int:
     total = 0
+    for rank, hand in enumerate(sorted(map(JokerHand, data.splitlines()), key=cmp_to_key(JokerHand.cmp)), 1):
+        total += (rank * hand.bid)
+        print(rank, hand.cards, hand.type, hand.bid)
+    return total
 
 
 if __name__ == '__main__':
--- Day07/test_day07.py
+++ Day07/test_day07.py
@@ -1,4 +1,5 @@
 import unittest
+from itertools import permutations
 from textwrap import dedent
 
 from day07 import *
@@ -10,31 +11,88 @@ data = dedent("""\
         KTJJT 220
         QQQJA 483""")
 
-#         6: r'(.)\1{4,}',
-#         5: r'(.)\1{3,}',
-#         4: r'(?:(.)\1{1,}(.)\2{2,}|(.)\1{2,}(.)\2{1,})',
-#         3: r'(.)\1{2,}',
-#         2: r'(.)\1{1,}.?(.)\2{1,}',
-#         1: r'(.)\1{1,}',
-#         0: r'\w{5}',
+FIVE_OF_A_KIND = 6
+FOUR_OF_A_KIND = 5
+FULL_HOUSE = 4
+THREE_OF_A_KIND = 3
+TWO_PAIR = 2
+TWO_OF_A_KIND = 1
+HIGH_CARD = 0
 
 
 class TestDay07(unittest.TestCase):
 
-    def test_cards(self):
-        self.assertEqual(Hand('AAAAA 1').type, 6)
-        self.assertEqual(Hand('AAAA2 1').type, 5)
-        self.assertEqual(Hand('AAA22 1').type, 4)
-        self.assertEqual(Hand('AAA23 1').type, 3)
-        self.assertEqual(Hand('AA223 1').type, 2)
-        self.assertEqual(Hand('AA234 1').type, 1)
-        self.assertEqual(Hand('A2345 1').type, 0)
+    def test_hand(self):
+        self.assertEqual(Hand('AAAAA 1').type, FIVE_OF_A_KIND)
+        self.assertEqual(Hand('AAAA2 1').type, FOUR_OF_A_KIND)
+        self.assertEqual(Hand('AAA22 1').type, FULL_HOUSE)
+        self.assertEqual(Hand('AAA23 1').type, THREE_OF_A_KIND)
+        self.assertEqual(Hand('AA223 1').type, TWO_PAIR)
+        self.assertEqual(Hand('AA234 1').type, TWO_OF_A_KIND)
+        self.assertEqual(Hand('A2345 1').type, HIGH_CARD)
+
+    def test_jokerhands(self):
+        # 5 Jokers
+        self.assertEqual(JokerHand('JJJJJ 1').type, FIVE_OF_A_KIND)
+
+        # 4 Jokers
+        self.assertEqual(JokerHand('JJJJA 1').type, FIVE_OF_A_KIND)
+
+        # 3 Jokers
+        self.assertEqual(JokerHand('JJJAA 1').type, FIVE_OF_A_KIND)
+        self.assertEqual(JokerHand('JJJA2 1').type, FOUR_OF_A_KIND)
+
+        # 2 Jokers
+        self.assertEqual(JokerHand('JJAAA 1').type, FIVE_OF_A_KIND)
+        self.assertEqual(JokerHand('JJAA2 1').type, FOUR_OF_A_KIND)
+        self.assertEqual(JokerHand('JJA23 1').type, THREE_OF_A_KIND)
+
+        # 1 Joker
+        self.assertEqual(JokerHand('JAAAA 1').type, FIVE_OF_A_KIND)
+        self.assertEqual(JokerHand('JAAA2 1').type, FOUR_OF_A_KIND)
+        self.assertEqual(JokerHand('JAA22 1').type, FULL_HOUSE)
+        self.assertEqual(JokerHand('JAA23 1').type, THREE_OF_A_KIND)
+        self.assertEqual(JokerHand('JA234 1').type, TWO_OF_A_KIND)
+
+        # 0 Jokers
+        self.assertEqual(JokerHand('AAAAA 1').type, FIVE_OF_A_KIND)
+        self.assertEqual(JokerHand('AAAA2 1').type, FOUR_OF_A_KIND)
+        self.assertEqual(JokerHand('AAA22 1').type, FULL_HOUSE)
+        self.assertEqual(JokerHand('AAA23 1').type, THREE_OF_A_KIND)
+        self.assertEqual(JokerHand('AA223 1').type, TWO_PAIR)
+        self.assertEqual(JokerHand('AA234 1').type, TWO_OF_A_KIND)
+        self.assertEqual(JokerHand('A2345 1').type, HIGH_CARD)
+
+        map = {'JJJJJ': FIVE_OF_A_KIND,
+               'JJJJA': FIVE_OF_A_KIND,
+               'JJJAA': FIVE_OF_A_KIND,
+               'JJJA2': FOUR_OF_A_KIND,
+               'JJAAA': FIVE_OF_A_KIND,
+               'JJAA2': FOUR_OF_A_KIND,
+               'JJA23': THREE_OF_A_KIND,
+               'JAAAA': FIVE_OF_A_KIND,
+               'JAAA2': FOUR_OF_A_KIND,
+               'JAA22': FULL_HOUSE,
+               'JAA23': THREE_OF_A_KIND,
+               'JA234': TWO_OF_A_KIND,
+               'AAAAA': FIVE_OF_A_KIND,
+               'AAAA2': FOUR_OF_A_KIND,
+               'AAA22': FULL_HOUSE,
+               'AAA23': THREE_OF_A_KIND,
+               'AA223': TWO_PAIR,
+               'AA234': TWO_OF_A_KIND,
+               'A2345': HIGH_CARD}
+
+        for hand, expected in map.items():
+            for perm in permutations(hand, 5):
+                # print('hand, expected:', hand, expected)
+                self.assertEqual(JokerHand(''.join(perm) + ' 1').type, expected)
 
     def test_part1(self):
         self.assertEqual(6440, part_1(data))
 
     def test_part2(self):
-        self.assertEqual(None, part_2(data))
+        self.assertEqual(5905, part_2(data))
 
 
 if __name__ == '__main__':
