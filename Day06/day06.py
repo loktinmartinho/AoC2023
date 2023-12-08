913c0c1464618ea9eb4bf438c5f5910b38af7635
Advic <loktinmartinho@gmail.com>
2023-12-07 21:20:37 -0800

    Day 6 move & bruteforce part 2

--- Day06/day06.py
+++ Day06/day06.py
@@ -45,6 +45,24 @@ def part_2(data: str) -> int:
     return int(t1) - int(t0)
 
 
+def part_2_brute(data: str) -> int:
+    lines = data.splitlines()
+    times = int(''.join(lines[0].split()[1:]))
+    distances = int(''.join(lines[1].split()[1:]))
+    records = [Record(times, distances)]
+    total = 1
+    for record in records:
+        racetime = record.time
+        beats = 0
+        for holdtime in range(racetime):
+            distance = holdtime * (racetime - holdtime)
+            if distance > record.distance:
+                beats += 1
+        total *= beats
+    return total
+
+
 if __name__ == '__main__':
     print(part_1(open('data.txt').read()))
     print(part_2(open('data.txt').read()))
+    print(part_2_brute(open('data.txt').read()))
